8067e7e56127334ae2a8e2a60fce0d46f073723e
Pedro A. Aranda Gutierrez <paaguti@gmail.com>
2023-02-05 11:01:02 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # tramp-interactive
 tramp-interactive: An easier find-file for TRAMP
+
+This module provides a comfortable means to build the file name for `find-file` when accessing remote servers using ssh and TRAMP.
+
+Once installed in your Emacs configuration directory tree, add it using `use-package`:
+
+```
+(use-package tramp-interactive
+  :ensure nil
+  :load-path "lisp/tramp-interactive"
+  :pin manual
+  :init
+  (require 'tramp)
+  ;; Just in case, make sure that the LSP is there
+  (add-to-list 'tramp-remote-path "~/.local/bin")
+  :config
+  (message "Configuring tramp-interactive...")
+  (setq tramp-default-user (getenv "USER"))
+  (setq tramp-user-list `(,tramp-default-user "student"))
+  (setq tramp-host-list `("localhost" "osm12.local")))
+```
+
+Variables to customise your environment:
+
+`tramp-default-user` will normally be your user in the system. When you access a remote server with this user, you tramp-interactive will suppress the `user@` part in the file spec
+
+`tramp-user-list` is a list of frequently used user names that will be used to auto-complete the user prompt.
+
+`tramp-host-list` is a list of frequently used hosts
+
+Key bindings:
+
+`C-x T f` to access an (eventually) port translated remote server.
+
+`C-x T l` to access a port translated virtual machine in the current host.
---
+++ tramp-interactive.el
@@ -0,0 +1,75 @@
+;;; tramp-interactive.el --- Find files with tramp easier
+
+;; Copyright (C) 2022 Pedro A. Aranda Gutiérrez
+
+;; Author: Pedro A. Aranda Gutiérrez
+;; Version: 1.0
+;; Package-Requires:
+;; Keywords: tramp, find-file
+;; URL:
+
+;;; Commentary:
+;;
+;; This package provides two functions to make
+;; find-file easier when files are accessed with TRAMP.
+;; It asks for user, host and port and then
+;; creates the initial value to delegate to `find-file'
+;;
+;; Keybindings
+;;
+;; C-x T f : find file in remote server
+;; C-x T l : find file in local server
+
+(provide 'tramp-interactive)
+(require 'tramp)
+
+
+;;;###autoload
+(defvar tramp-default-user (getenv "USER")
+  "The default user for TRAMP")
+;;;###autoload
+(defvar tramp-user-list (list (getenv "USER"))
+  "The list of users you use frequently when accessing remote files")
+;;;###autoload
+(defvar tramp-host-list (list "localhost")
+  "The list of users you use frequently when accessing remote files")
+
+(defun tramp-initial-spec (user host port)
+  (let ((initial (if (string= user tramp-default-user)
+                  (format "/ssh:%s" host)
+                  (format "/ssh:%s@%s" user host))))
+    (setq initial (concat initial (if (= port 22) ":"
+				    (format "#%d:" port))))
+    initial))
+
+;;;###autoload
+(defun tramp-open-local (filename)
+  "Open a file in a port-xlated VM
+default user is student"
+  (interactive
+   (let*
+       ((prompt-user (format "user (%s): " tramp-default-user))
+	(user (completing-read prompt-user tramp-user-list nil nil nil nil tramp-default-user))
+	(port (read-number "port: ")))
+     (list (read-file-name "Remote file:" (tramp-initial-spec user "localhost" port) nil 'confirm ))))
+  (find-file filename))
+
+;;;###autoload
+(defun tramp-open-remote (filename)
+  "Open a file in a potentially port-traslated remote host
+default user is student"
+  (interactive
+   (let*
+       ((default-host (car tramp-host-list))
+	(prompt-host (format "host (%s): " default-host))
+	(prompt-user (format "user (%s): " tramp-default-user))
+	(host ;; (read-string "host: "))
+	 (completing-read prompt-host tramp-host-list nil nil nil nil default-host ))
+	(user (completing-read prompt-user tramp-user-list nil nil nil nil tramp-default-user))
+	(port (read-number "port: " 22)))
+     (list (read-file-name "Remote file:" (tramp-initial-spec user host port) nil 'confirm ))))
+  (find-file filename))
+
+(keymap-global-set "C-x T l" 'tramp-open-local)
+(keymap-global-set "C-x T f" 'tramp-open-remote)
+;;; tramp-interactive.el ends here
